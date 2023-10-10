235731907a10d5242b6b0d867f951d7c8628bb21
anas0alnajjar <anas0alnajjar@gmail.com>
2023-10-11 06:10:02 +0630

    I add script client for fetch some value

--- aqhc/aqhc/doctype/key_people/key_people.js
+++ aqhc/aqhc/doctype/key_people/key_people.js
@@ -1,8 +1,19 @@
 // Copyright (c) 2023, anas and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('KEY PEOPLE', {
-	// refresh: function(frm) {
+frappe.ui.form.on('keys',{
+form_render: function(frm,cdt,cdn){
+    let item = locals[cdt][cdn]
+    item.code_city=frm.doc.ccity
+    frm.refresh_field('tb_keys')
+}
+});
 
-	// }
+
+frappe.ui.form.on('school',{
+form_render: function(frm,cdt,cdn){
+    let item = locals[cdt][cdn]
+    item.for_city=frm.doc.ccity
+    frm.refresh_field('tb_school')
+}
 });
